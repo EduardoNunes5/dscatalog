17f816c3fdd0df4d7f070820ee623abb214e9c45
eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2022-01-19 14:01:27 -0300

    delete category

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.resources.exceptions;
 
+import com.devsuperior.dscatalog.services.exception.DatabaseException;
 import com.devsuperior.dscatalog.services.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +15,26 @@ public class ResourceExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest req){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError error = buildError(e.getMessage(), req.getRequestURI(), status, "Resource not found");
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> databaseException(ResourceNotFoundException e, HttpServletRequest req){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError error = buildError(e.getMessage(), req.getRequestURI(), status, "Database Exception.");
+        return ResponseEntity.status(status).body(error);
+    }
+
+    private StandardError buildError(String msg, String path, HttpStatus status, String errorStr){
         StandardError error = new StandardError();
         error.setTimestamp(Instant.now());
-        error.setMessage(e.getMessage());
-        error.setPath(req.getRequestURI());
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setError("Resource not found");
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+        error.setMessage(msg);
+        error.setPath(path);
+        error.setStatus(status.value());
+        error.setError(errorStr);
+        return error;
     }
+
 }
